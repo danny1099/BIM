9c37b26ee5a7bf545529c549eca62a7d080c37be
danny1099 <danny.mosquera.1099@gmail.com>
2021-04-22 17:26:13 -0500

    update 16

--- BIM/UI/0. Initial/start_initial_home.Designer.vb
+++ BIM/UI/0. Initial/start_initial_home.Designer.vb
@@ -19,13 +19,27 @@ Partial Class start_initial_home
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.LabelControl1 = New DevExpress.XtraEditors.LabelControl()
         Me.SuspendLayout()
+        '
+        'LabelControl1
+        '
+        Me.LabelControl1.Appearance.ForeColor = System.Drawing.Color.LightSeaGreen
+        Me.LabelControl1.Appearance.Options.UseForeColor = True
+        Me.LabelControl1.Location = New System.Drawing.Point(26, 23)
+        Me.LabelControl1.LookAndFeel.SkinName = "Office 2019 White"
+        Me.LabelControl1.LookAndFeel.UseDefaultLookAndFeel = False
+        Me.LabelControl1.Name = "LabelControl1"
+        Me.LabelControl1.Size = New System.Drawing.Size(19, 16)
+        Me.LabelControl1.TabIndex = 0
+        Me.LabelControl1.Text = "BIM"
         '
         'start_initial_home
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(96.0!, 96.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
-        Me.ClientSize = New System.Drawing.Size(752, 305)
+        Me.ClientSize = New System.Drawing.Size(752, 494)
+        Me.Controls.Add(Me.LabelControl1)
         Me.IconOptions.Image = Global.BIM.My.Resources.Resources.bim_ico
         Me.LookAndFeel.SkinName = "Office 2019 White"
         Me.LookAndFeel.UseDefaultLookAndFeel = False
@@ -35,6 +49,9 @@ Partial Class start_initial_home
         Me.Text = "BIM"
         Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents LabelControl1 As DevExpress.XtraEditors.LabelControl
 End Class
